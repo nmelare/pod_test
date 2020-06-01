cde89564598801bed35c0a449597d905d29f6373
Nathalia <nathaliamelare@gmail.com>
2020-06-01 19:47:10 -0300

    Add proj examples

---
+++ pod_test_/pod_test/pod_test.h
@@ -0,0 +1,19 @@
+//
+//  pod_test.h
+//  pod_test
+//
+//  Created by Nathalia Melare on 01/06/20.
+//  Copyright © 2020 Nathalia Melare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for pod_test.
+FOUNDATION_EXPORT double pod_testVersionNumber;
+
+//! Project version string for pod_test.
+FOUNDATION_EXPORT const unsigned char pod_testVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pod_test/PublicHeader.h>
+
+
