350c61c74d34fda4f2fb22fb1de0694b48a4d987
Nathalia <nathaliamelare@gmail.com>
2020-06-02 22:23:30 -0300

    Pod created

---
+++ pod_test_/pod_test.podspec
@@ -0,0 +1,40 @@
+Pod::Spec.new do |s|
+
+# 1
+s.platform = :ios
+s.ios.deployment_target = '12.0'
+s.name = "pod_test"
+s.summary = "pod_test lets a user call someone."
+s.requires_arc = true
+
+# 2
+s.version = "0.1.0"
+
+# 3
+s.license = { :type => "MIT", :file => "LICENSE" }
+
+# 4 - Replace with your name and e-mail address
+s.author = { "Nathalia  Melare" => "nathaliamelare@gmail.com" }
+
+# 5 - Replace this URL with your own GitHub page's URL (from the address bar)
+s.homepage = "https://github.com/nmelare/pod_test"
+
+# 6 - Replace this URL with your own Git URL from "Quick Setup"
+s.source = { :git => "https://github.com/nmelare/pod_test.git",
+             :tag => "#{s.version}" }
+
+# 7
+s.framework = "UIKit"
+s.dependency 'Alamofire', '~> 4.7'
+s.dependency 'MBProgressHUD', '~> 1.1.0'
+
+# 8
+s.source_files = "pod_test/**/*.{swift}"
+
+# 9
+s.resources = "pod_test/**/*.{png,jpeg,jpg,storyboard,xib,xcassets}"
+
+# 10
+s.swift_version = "4.2"
+
+end
---
+++ pod_test_/pod_test/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Call_Test
+//
+//  Created by Nathalia Melare on 01/06/20.
+//  Copyright © 2020 Nathalia Melare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var phone: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpButton()
+    }
+    
+    func setUpButton() {
+        let button = UIButton(type: .system)
+        button.frame = CGRect(x: self.view.frame.width/3, y: self.view.frame.height/2, width: 150, height: 75)
+        button.backgroundColor = .systemGreen
+        button.setTitle("Call", for: UIControl.State.normal)
+        button.setTitleColor(.white, for: UIControl.State.normal)
+        button.titleLabel?.font = .systemFont(ofSize: 40)
+        button.addTarget(self, action: #selector(call(sender:)), for: UIControl.Event.touchDown)
+        self.view.addSubview(button)
+    }
+    
+    @objc func call(sender: UIButton!) {
+        if let url = URL(string: "tel://\(phone)"),
+            UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
+    
+}
+
